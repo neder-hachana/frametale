84515bc9c02a32874f2d43c98dbfc89f196174e9
neder-hachana <mohamedneder.hachana@frametale.com>
2025-03-23 11:42:27 +0100

    Added web icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import HomePage from "./pages/HomePage"
 import './App.css';
-import { Helmet } from 'react-helmet';
 
 function App() {
 
   return (
     <>
-      <Helmet>
-        <title>Frametale - AI-Powered Photo Products</title>
-        <meta
-          name="description"
-          content="Frametale transforms your memories into stunning AI-designed photobooks, calendars, and cards. Create effortlessly and cherish forever. Join the waitlist for exclusive offers!"
-        />
-      </Helmet>
       <HomePage />
     </>
   )
